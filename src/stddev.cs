7fa34e1b62c5bf57425f0e38bcb03b4ea0a339b1
Antonín Štoll <antoninstoll@gmail.com>
2021-04-27 19:44:19 +0200

    Přidání stddev

---
+++ src/stddev.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace stddev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj svìte!");
+            Console.ReadKey();
+        }
+    }
+}
